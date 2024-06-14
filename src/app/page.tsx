b400385b3177df58c5b5cc5c66cc33ba7740924e
Cassia <125088421+cassiab13@users.noreply.github.com>
2024-06-13 23:51:00 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,8 @@ export default function Home() {
         <ul>
           <li>Squirtle é um dos Pokémon mais populares e adoráveis.</li>
           <li>
-            Seu nome deriva das palavras "squirrel" (esquilo) e "turtle"
-            (tartaruga).
+            Seu nome deriva das palavras "squirrel" `esquilo e "turtle"
+            tartaruga.
           </li>
           <li>
             Squirtle é frequentemente escolhido por treinadores para começar sua
